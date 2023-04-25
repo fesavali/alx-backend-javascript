4ae64b7f2331b57e235aa01f5f497239f9201d37
fesavali <felixnzioki99@gmail.com>
2023-04-25 12:02:06 +0300

    weak

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,15 @@
+const queryAPI = (endpoint) => {
+    let times = weakMap.get(endpoint) || 0;
+  
+    times += 1;
+  
+    weakMap.set(endpoint, times);
+  
+    if (times >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  
+    return times;
+  };
+  
+  export { weakMap, queryAPI };
